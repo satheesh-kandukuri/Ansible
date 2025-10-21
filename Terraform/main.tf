7c31070e8ba08d1836617ca28f7b2f5f2cc2a2bc
Kandukuri Satheesh <kandukuri.satheesh@autodesk.com>
2025-10-21 07:40:32 +0530

    Commit - 09

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -68,6 +68,7 @@ resource "aws_db_instance" "mysql" {
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   publicly_accessible    = false
   skip_final_snapshot    = true
+  availability_zone      = "us-west-2a"
 }
 
 
